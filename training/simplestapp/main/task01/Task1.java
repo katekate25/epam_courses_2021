acb00401be059ead9e833a3be33d3d863cb4adfd
mironenka.katsiaryna <mironenka.katsiaryna.i@gmail.com>
2021-09-29 14:27:06 +0300

    add comment

--- training/simplestapp/main/task01/Task1.java
+++ training/simplestapp/main/task01/Task1.java
@@ -1,6 +1,8 @@
 package by.training.simplestapp.main.task01;
 import java.util.Scanner;
 
+//Составить линейную программу, печатающую значение true, если указанное высказывание является истинным, и false — в противном случае:
+//Сумма двух первых цифр заданного четырехзначного числа равна сумме двух его последних цифр.
 
 public class Task1 {
     public static void main(String[] args) {
